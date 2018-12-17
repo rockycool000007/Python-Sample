672de7eb48410aaf03e3ba92d2511a39632d6d61
rockycool000007 <44780407+rockycool000007@users.noreply.github.com>
2018-12-17 00:19:20 -0800

    updated run.py

--- run.py
+++ run.py
@@ -3,14 +3,11 @@ import sys
 def addNumbers(num1, num2):
     return num1 + num2
 
-
 def subNumbers(num1, num2):
     return num1 - num2
 
-
 def mulNumbers(num1, num2):
-    return num1 * num2
-
+    return num1 * num2  
 
 def divNumbers(num1, num2):
     return num1 / num2
